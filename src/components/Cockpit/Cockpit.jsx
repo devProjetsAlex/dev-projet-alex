c81016adc1679d55934967356e355eab81cacd02
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-03-03 09:32:42 -0500

    convert to classes

--- src/components/Cockpit/Cockpit.jsx
+++ src/components/Cockpit/Cockpit.jsx
@@ -31,7 +31,7 @@ const cockpit = (props) => {
 
     return (
     <div>
-        <h1>Hi, I'm a React App</h1>
+        <h1>{props.appTitle}</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <StyledButton alt={props.showPersons} onClick={props.clicked}>
           Toggle Persons
